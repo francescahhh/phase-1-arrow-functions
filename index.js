e57928c522c8d9661959de4fbf2040cd3158afab
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-03 20:21:15 -0700

    commit tree c49890602aa4f0b3fa222730c8316b3c5d748775

---
+++ index.js
@@ -0,0 +1,25 @@
+const divide = function() {
+    let num1 = 2000;
+    let num2 = 100;
+    return num1 / num2;
+}
+
+//
+
+const square = x => x ** x;
+  
+//    
+
+const add = (a, b) => a + b;  
+
+//     it("has an arrow function called add", () => {
+//       expect(add).to.exist
+//     })
+  
+//     it("add arrow function takes two parameters and adds them together", () => {
+//       let a = 3
+//       let b = 4
+//       expect(add(a,b)).to.equal(a+b)
+//     })
+//   })
+  
